759c8c89d9b9e59dd0b62672e1f8e52c9629eda2
alexmpa <62516783+alexmpa@users.noreply.github.com>
2020-09-22 18:54:26 +0300

    Update Neo RPCs

--- Assets/Neo/Utils/NeoRPCs.cs
+++ Assets/Neo/Utils/NeoRPCs.cs
@@ -8,15 +8,16 @@ namespace Phantasma.Neo.Utils
         {
             return new List<string> {
                 "http://jpc.phantasma.io:10332",
-                "https://seed0.cityofzion.io:443",
-                "https://seed6.cityofzion.io:443",
-                "https://seed7.cityofzion.io:443",
-                "https://seed8.cityofzion.io:443",
-                "https://seed9.cityofzion.io:443",
-                "https://seed1.switcheo.network:10331",
-                "https://seed2.switcheo.network:10331",
-                "https://seed3.switcheo.network:10331",
-                "http://seed.neoeconomy.io:10332"
+                "http://seed.neoeconomy.io:10332",
+                "http://seed1.ngd.network:10332",
+                "http://seed2.ngd.network:10332",
+                "http://seed3.ngd.network:10332",
+                "http://seed4.ngd.network:10332",
+                "http://seed5.ngd.network:10332",
+                "http://seed6.ngd.network:10332",
+                "http://seed7.ngd.network:10332",
+                "http://seed8.ngd.network:10332",
+                "http://seed9.ngd.network:10332"
             };
         }
     }
