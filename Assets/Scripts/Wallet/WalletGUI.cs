faa7bfb70495b764c57c8cbc7243c5768a92a3e7
alexmpa <62516783+alexmpa@users.noreply.github.com>
2020-10-15 21:32:24 +0300

    Error prone seed phrase import
    https://phantasma.atlassian.net/browse/POL-136

--- Assets/Scripts/Wallet/WalletGUI.cs
+++ Assets/Scripts/Wallet/WalletGUI.cs
@@ -1639,7 +1639,9 @@ namespace Poltergeist
                                     else
                                     if (key.Count(x => x == ' ') == 11)
                                     {
-                                        ShowModal("Seed import", "Enter seed password.\nIf you put a wrong password, the wrong public address will be generated.\n\nFor wallets created with v1.3 or later, it is ok to leave this blank.\n", ModalState.Input, 0, 64, ModalConfirmCancel, 1, (seedResult, password) =>
+                                        ShowModal("Seed import",
+                                            "For wallets created with Poltergeist v1.0-v1.2: Enter seed password.\nIf you put a wrong password, the wrong public address will be generated.\n\nNOTE: For wallets created with v1.3 or later (without a seed password), you must leave this field blank.\nThis is NOT your wallet password used to log into the wallet.\n",
+                                            ModalState.Input, 0, 64, ModalConfirmCancel, 1, (seedResult, password) =>
                                         {
                                             if (seedResult != PromptResult.Success)
                                             {
