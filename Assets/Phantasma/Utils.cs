272ae281e1b9f23eadd4bd02208b0fe5e06e9014
alexmpa <62516783+alexmpa@users.noreply.github.com>
2022-09-26 01:04:09 +0300

    Drop unused code

--- Assets/Phantasma/Utils.cs
+++ Assets/Phantasma/Utils.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 
 namespace Poltergeist.PhantasmaLegacy.Core.Utils
 {
     public static class Utils
     {
-        public static string ToTitleCase(this string str)
-        {
-            var tokens = str.Split(new[] { " ", "-" }, StringSplitOptions.RemoveEmptyEntries);
-            for (var i = 0; i < tokens.Length; i++)
-            {
-                var token = tokens[i];
-                tokens[i] = char.ToUpper(token[0]) + token.Substring(1).ToLower();
-            }
-
-            return string.Join(" ", tokens);
-        }
-
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static uint ToUInt32(this byte[] value, int startIndex)
         {
@@ -27,58 +12,6 @@ namespace Poltergeist.PhantasmaLegacy.Core.Utils
             var c = value[startIndex]; startIndex++;
             var d = value[startIndex]; startIndex++;
             return (uint)(a + (b << 8) + (c << 16) + (d << 24));
-        }
-
-        internal static long WeightedAverage<T>(this IEnumerable<T> source, Func<T, long> valueSelector, Func<T, long> weightSelector)
-        {
-            long sum_weight = 0;
-            long sum_value = 0;
-            foreach (T item in source)
-            {
-                long weight = weightSelector(item);
-                sum_weight += weight;
-                sum_value += valueSelector(item) * weight;
-            }
-            if (sum_value == 0) return 0;
-            return sum_value / sum_weight;
-        }
-
-        internal static IEnumerable<TResult> WeightedFilter<T, TResult>(this IList<T> source, double start, double end, Func<T, long> weightSelector, Func<T, long, TResult> resultSelector)
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (start < 0 || start > 1) throw new ArgumentOutOfRangeException(nameof(start));
-            if (end < start || start + end > 1) throw new ArgumentOutOfRangeException(nameof(end));
-            if (weightSelector == null) throw new ArgumentNullException(nameof(weightSelector));
-            if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
-            if (source.Count == 0 || start == end) yield break;
-            double amount = source.Sum(weightSelector);
-            long sum = 0;
-            double current = 0;
-            foreach (T item in source)
-            {
-                if (current >= end) break;
-                long weight = weightSelector(item);
-                sum += weight;
-                double old = current;
-                current = sum / amount;
-                if (current <= start) continue;
-                if (old < start)
-                {
-                    if (current > end)
-                    {
-                        weight = (long)((end - start) * amount);
-                    }
-                    else
-                    {
-                        weight = (long)((current - start) * amount);
-                    }
-                }
-                else if (current > end)
-                {
-                    weight = (long)((end - old) * amount);
-                }
-                yield return resultSelector(item, weight);
-            }
         }
     }
 }
