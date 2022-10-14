690166d053ca5ae8f9a08d0fd66660e90d64b015
alexmpa <62516783+alexmpa@users.noreply.github.com>
2022-10-14 17:41:45 +0300

    Fix dynamic RPC selection for new PHA testnet

--- Assets/Scripts/Wallet/AccountManager.cs
+++ Assets/Scripts/Wallet/AccountManager.cs
@@ -519,16 +519,12 @@ namespace Poltergeist
 
                             if (String.IsNullOrEmpty(Settings.phantasmaRPCURL))
                             {
-                                // Checking if we are still on mainnet
-                                if (Settings.nexusKind == NexusKind.Main_Net)
-                                {
-                                    // If we have no previously used RPC, we select random one at first.
-                                    var index = ((int)(Time.realtimeSinceStartup * 1000)) % rpcNumberPhantasma;
-                                    var node = response.GetNodeByIndex(index);
-                                    var result = node.GetString("url") + "/rpc";
-                                    Settings.phantasmaRPCURL = result;
-                                    Log.Write($"Changed Phantasma RPC url {index} => {result}");
-                                }
+                                // If we have no previously used RPC, we select random one at first.
+                                var index = ((int)(Time.realtimeSinceStartup * 1000)) % rpcNumberPhantasma;
+                                var node = response.GetNodeByIndex(index);
+                                var result = node.GetString("url") + "/rpc";
+                                Settings.phantasmaRPCURL = result;
+                                Log.Write($"Changed Phantasma RPC url {index} => {result}");
                             }
 
                             UpdateAPIs();
@@ -552,8 +548,8 @@ namespace Poltergeist
 
                                         if (rpcBenchmarkedPhantasma == rpcNumberPhantasma)
                                         {
-                                        // We finished benchmarking, time to select best RPC server.
-                                        TimeSpan bestTime;
+                                            // We finished benchmarking, time to select best RPC server.
+                                            TimeSpan bestTime;
                                             string bestRpcUrl = GetFastestWorkingRPCURL(PlatformKind.Phantasma, out bestTime);
 
                                             if (String.IsNullOrEmpty(bestRpcUrl))
@@ -564,13 +560,9 @@ namespace Poltergeist
                                             {
                                                 Log.Write($"Fastest Phantasma RPC is {bestRpcUrl}: {new DateTime(bestTime.Ticks).ToString("ss.fff")} sec.");
 
-                                            // Checking if we are still on mainnet
-                                            if (Settings.nexusKind == NexusKind.Main_Net)
-                                                {
-                                                    Settings.phantasmaRPCURL = bestRpcUrl;
-                                                    UpdateAPIs();
-                                                    Settings.SaveOnExit();
-                                                }
+                                                Settings.phantasmaRPCURL = bestRpcUrl;
+                                                UpdateAPIs();
+                                                Settings.SaveOnExit();
                                             }
                                         }
                                     },
@@ -587,8 +579,8 @@ namespace Poltergeist
 
                                         if (rpcBenchmarkedPhantasma == rpcNumberPhantasma)
                                         {
-                                        // We finished benchmarking, time to select best RPC server.
-                                        TimeSpan bestTime;
+                                            // We finished benchmarking, time to select best RPC server.
+                                            TimeSpan bestTime;
                                             string bestRpcUrl = GetFastestWorkingRPCURL(PlatformKind.Phantasma, out bestTime);
 
                                             if (String.IsNullOrEmpty(bestRpcUrl))
@@ -597,14 +589,10 @@ namespace Poltergeist
                                             }
                                             else
                                             {
-                                            // Checking if we are still on mainnet
-                                            if (Settings.nexusKind == NexusKind.Main_Net)
-                                                {
-                                                    Log.Write($"Fastest Phantasma RPC is {bestRpcUrl}: {new DateTime(bestTime.Ticks).ToString("ss.fff")} sec.");
-                                                    Settings.phantasmaRPCURL = bestRpcUrl;
-                                                    UpdateAPIs();
-                                                    Settings.SaveOnExit();
-                                                }
+                                                Log.Write($"Fastest Phantasma RPC is {bestRpcUrl}: {new DateTime(bestTime.Ticks).ToString("ss.fff")} sec.");
+                                                Settings.phantasmaRPCURL = bestRpcUrl;
+                                                UpdateAPIs();
+                                                Settings.SaveOnExit();
                                             }
                                         }
                                     })
